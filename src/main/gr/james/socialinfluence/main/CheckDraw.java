ae00e857e65e200d0063f753aba59575ce7cf282
Giorgos Stamatelatos <gstamatelat@gmail.com>
2015-06-29 03:50:09 +0300

    Random game tests

--- src/main/gr/james/socialinfluence/main/CheckDraw.java
+++ src/main/gr/james/socialinfluence/main/CheckDraw.java
@@ -1,34 +1,18 @@
 package gr.james.socialinfluence.main;
 
+import gr.james.socialinfluence.game.GameDefinition;
+import gr.james.socialinfluence.game.players.BruteForcePlayer;
+import gr.james.socialinfluence.game.players.Player;
 import gr.james.socialinfluence.graph.Graph;
 import gr.james.socialinfluence.graph.MemoryGraph;
-import gr.james.socialinfluence.graph.generators.TwoWheels;
+import gr.james.socialinfluence.graph.generators.Wheel;
 
 import java.io.IOException;
 
 public class CheckDraw {
     public static void main(String[] args) throws IOException {
-        /*Graph g = Csv.from(new URL("http://loki.ee.duth.gr/school.csv").openStream());
-
-        Game game = new Game(g);
-
-        Move m1 = new Move();
-        m1.putVertex(g.getVertexFromIndex(56), 1.017341);
-        m1.putVertex(g.getVertexFromIndex(191), 0.982659);
-
-        Move m2 = new Move();
-        m2.putVertex(g.getVertexFromIndex(56), 0.830000);
-        m2.putVertex(g.getVertexFromIndex(237), 1.170000);
-
-        GameDefinition d = new GameDefinition(2, 2.0, 0, false);
-
-        game.setPlayer(PlayerEnum.A, m1);
-        game.setPlayer(PlayerEnum.B, m2);
-
-        GameResult gr = game.runGame(d, 1.0e-5);
-
-        Helper.log("%s", gr);*/
-        Graph g = TwoWheels.generate(MemoryGraph.class, 4);
-        System.out.println(g);
+        Graph g = Wheel.generate(MemoryGraph.class, 15);
+        Player p = new BruteForcePlayer();
+        p.findMove(g, new GameDefinition(3, 3.0, 0, false));
     }
 }
