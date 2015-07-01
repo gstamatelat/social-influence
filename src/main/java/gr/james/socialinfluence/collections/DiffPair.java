06a9bf2a5513324f0f32515da6451b89580f8ec6
Giorgos Stamatelatos <gstamatelat@gmail.com>
2015-07-01 07:25:15 +0300

    Split Pair and DiffPair

---
+++ src/main/java/gr/james/socialinfluence/collections/DiffPair.java
@@ -0,0 +1,69 @@
+package gr.james.socialinfluence.collections;
+
+import gr.james.socialinfluence.helper.Finals;
+
+/**
+ * <p>Stores exactly 2 non-null objects and is not mutable. They respect {@link #equals(Object)} and {@link #hashCode()}
+ * and may be used as indices or map keys. Note that they do not protect from malevolent behavior: if one or another
+ * object in the tuple is mutable, then it can be changed with the usual bad effects.</p>
+ */
+public class DiffPair<K, V> {
+    private K first;
+    private V second;
+
+    /**
+     * <p>Creates a {@code Pair} from the specified elements.</p>
+     *
+     * @param value1 the first value in the new {@code Pair}
+     * @param value2 the second value in the new {@code Pair}
+     * @throws IllegalArgumentException if either argument is {@code null}
+     */
+    public DiffPair(K value1, V value2) {
+        if (value1 == null || value2 == null) {
+            throw new IllegalArgumentException(Finals.E_PAIR_NULL);
+        }
+        first = value1;
+        second = value2;
+    }
+
+    /**
+     * <p>Returns the first element.</p>
+     *
+     * @return the first element
+     */
+    public K getFirst() {
+        return first;
+    }
+
+    /**
+     * <p>Returns the second element.</p>
+     *
+     * @return the second element
+     */
+    public V getSecond() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DiffPair<?, ?> diffPair = (DiffPair<?, ?>) o;
+
+        return first.equals(diffPair.first) && second.equals(diffPair.second);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = first.hashCode();
+        result = 31 * result + second.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("<%s, %s>", first, second);
+    }
+}
--- src/main/java/gr/james/socialinfluence/collections/Pair.java
+++ src/main/java/gr/james/socialinfluence/collections/Pair.java
@@ -1,69 +1,7 @@
 package gr.james.socialinfluence.collections;
 
-import gr.james.socialinfluence.helper.Finals;
-
-/**
- * <p>Stores exactly 2 non-null objects and is not mutable. They respect {@link #equals(Object)} and {@link #hashCode()}
- * and may be used as indices or map keys. Note that they do not protect from malevolent behavior: if one or another
- * object in the tuple is mutable, then it can be changed with the usual bad effects.</p>
- */
-public class Pair<T> {
-    private T first;
-    private T second;
-
-    /**
-     * <p>Creates a {@code Pair} from the specified elements.</p>
-     *
-     * @param value1 the first value in the new {@code Pair}
-     * @param value2 the second value in the new {@code Pair}
-     * @throws IllegalArgumentException if either argument is {@code null}
-     */
+public class Pair<T> extends DiffPair<T, T> {
     public Pair(T value1, T value2) {
-        if (value1 == null || value2 == null) {
-            throw new IllegalArgumentException(Finals.E_PAIR_NULL);
-        }
-        first = value1;
-        second = value2;
-    }
-
-    /**
-     * <p>Returns the first element.</p>
-     *
-     * @return the first element
-     */
-    public T getFirst() {
-        return first;
-    }
-
-    /**
-     * <p>Returns the second element.</p>
-     *
-     * @return the second element
-     */
-    public T getSecond() {
-        return second;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Pair<?> pair = (Pair<?>) o;
-
-        return first.equals(pair.first) && second.equals(pair.second);
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = first.hashCode();
-        result = 31 * result + second.hashCode();
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("<%s, %s>", first, second);
+        super(value1, value2);
     }
 }
