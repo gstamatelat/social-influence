e1f8c4b4f21ad2298ff6e7e5e917d715b8fdba43
gpanagiotidis <p-ioannis@hotmail.com>
2015-07-14 13:03:51 +0300

    Update Examples.java

--- src/main/java/gr/james/socialinfluence/main/Examples.java
+++ src/main/java/gr/james/socialinfluence/main/Examples.java
@@ -5,14 +5,27 @@ import gr.james.socialinfluence.api.GraphGenerator;
 import gr.james.socialinfluence.api.Player;
 import gr.james.socialinfluence.game.GameDefinition;
 import gr.james.socialinfluence.game.players.ExceptionPlayer;
+import gr.james.socialinfluence.graph.Edge;
 import gr.james.socialinfluence.graph.MemoryGraph;
+import gr.james.socialinfluence.graph.Vertex;
 import gr.james.socialinfluence.graph.generators.BarabasiAlbertGenerator;
+import gr.james.socialinfluence.graph.generators.RandomGenerator;
+import gr.james.socialinfluence.graph.io.Dot;
+
+import java.io.IOException;
+import java.util.Set;
 
 public class Examples {
-    public static void main(String[] args) {
-        GraphGenerator generator = new BarabasiAlbertGenerator<>(MemoryGraph.class, 25, 2, 2, 1.0);
-        Graph scaleFreeGraph = generator.create();
-        Player p = new ExceptionPlayer();
-        p.getMove(scaleFreeGraph, new GameDefinition(2, 2.0, 1000));
+    public static void main(String[] args) throws IOException {
+        Graph g = new MemoryGraph();
+        Vertex v1 = g.addVertex();
+        Vertex v2 = g.addVertex();
+        Vertex v3 = g.addVertex();
+        g.addEdge(v1, v2);
+        g.addEdge(v2, v3, true);
+        g.addEdge(v3, v1);
+
+        Dot dot = new Dot();
+        dot.to(g, System.out);
     }
 }
