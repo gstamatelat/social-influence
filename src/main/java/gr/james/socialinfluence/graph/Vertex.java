6e081c241e8876aa8b59dbd75be0109de4a36ccf
Giorgos Stamatelatos <gstamatelat@gmail.com>
2015-08-23 14:25:02 +0300

    Visibility and javadoc changes on Vertex.java

--- src/main/java/gr/james/socialinfluence/graph/Vertex.java
+++ src/main/java/gr/james/socialinfluence/graph/Vertex.java
@@ -1,6 +1,7 @@
 package gr.james.socialinfluence.graph;
 
 import gr.james.socialinfluence.api.Graph;
+import gr.james.socialinfluence.util.Conditions;
 
 /**
  * <p>Represents a single vertex. An object of type Vertex behaves like an immutable object, contains an id and a label.
@@ -11,11 +12,9 @@ import gr.james.socialinfluence.api.Graph;
  * exist in multiple graphs, if that makes sense for the application.</p>
  */
 public class Vertex implements Comparable<Vertex> {
-    /**
-     * <p>This field holds a serial number needed by {@link #getNextId getNextId()}.</p>
-     */
     private static int nextId = 1;
-    String label;
+
+    private String label;
     private int id;
 
     /**
@@ -47,7 +46,7 @@ public class Vertex implements Comparable<Vertex> {
     }
 
     /**
-     * <p>Returns the label of this vertex. Labels are strings that are used on printing and exporting.</p>
+     * <p>Returns the label of this vertex. Labels are strings that are used on printing, exporting etc.</p>
      *
      * @return the label of this vertex
      * @see #setLabel(String)
@@ -57,14 +56,15 @@ public class Vertex implements Comparable<Vertex> {
     }
 
     /**
-     * <p>Sets the label of this vertex. Labels are used on printing and exporting.</p>
+     * <p>Sets the label of this vertex. Labels are strings that are used on printing, exporting etc.</p>
      *
      * @param label the new label of the vertex
      * @return the current instance
+     * @throws NullPointerException if {@code label} is {@code null}
      * @see #getLabel()
      */
     public Vertex setLabel(String label) {
-        this.label = label;
+        this.label = Conditions.requireNonNull(label);
         return this;
     }
 
@@ -83,11 +83,22 @@ public class Vertex implements Comparable<Vertex> {
         return this.id;
     }
 
+    /**
+     * <p>Returns a string representation of this vertex, which is its label.</p>
+     *
+     * @return a string representation of this vertex
+     */
     @Override
     public String toString() {
         return this.label;
     }
 
+    /**
+     * <p>Compares this vertex with another vertex, using the underlying id as the comparator.</p>
+     *
+     * @param o the vertex to compare to
+     * @return {@code -1}, {@code 0} or {@code 1} if this vertex has a smaller, equal or bigger id than vertex {@code o} respectively
+     */
     @Override
     public int compareTo(Vertex o) {
         return Integer.compare(this.id, o.id);
