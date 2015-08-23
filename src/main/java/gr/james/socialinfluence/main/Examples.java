f5eb2772d365377c91cc5da65182ce0e66172c8b
Giorgos Stamatelatos <gstamatelat@gmail.com>
2015-08-24 01:32:00 +0300

    Implement HITS algorithm

--- src/main/java/gr/james/socialinfluence/main/Examples.java
+++ src/main/java/gr/james/socialinfluence/main/Examples.java
@@ -1,48 +1,20 @@
 package gr.james.socialinfluence.main;
 
-import gr.james.socialinfluence.algorithms.generators.RandomGenerator;
-import gr.james.socialinfluence.algorithms.scoring.PageRank;
+import gr.james.socialinfluence.algorithms.scoring.HITS;
 import gr.james.socialinfluence.api.Graph;
 import gr.james.socialinfluence.graph.MemoryGraph;
-import gr.james.socialinfluence.util.RandomHelper;
+import gr.james.socialinfluence.graph.Vertex;
 
 import java.io.IOException;
 
 public class Examples {
     public static void main(String[] args) throws IOException {
-        long now = System.currentTimeMillis();
-        for (int i = 0; i < 10000; i++) {
-
-            /*Graph g = new MemoryGraph();
-
-            List<Vertex> added = g.addVertices(4);
-
-            Vertex v1 = added.get(0);
-            Vertex v2 = added.get(1);
-            Vertex v3 = added.get(2);
-            Vertex v4 = added.get(3);
-
-            g.addEdge(v1, v3);
-            g.addEdge(v3, v2);
-            g.addEdge(v3, v4);
-            g.addEdge(v4, v1);
-            g.addEdge(v2, v1);*/
-
-
-            double dampingFactor = RandomHelper.getRandom().nextDouble();
-            double p = RandomHelper.getRandom().nextDouble();
-            int vertexCount = 40;
-
-            Graph g = new RandomGenerator<>(MemoryGraph.class, vertexCount, p).create();
-
-            /* PageRank */
-            //new Dot().to(g, System.out);
-            /*PageRank.execute(g, 0.0, 0.0, (oldState, newState) -> System.out.printf("%.17f,%.17f,%.17f,%.17f%n",
-                            newState.get(v1), newState.get(v2), newState.get(v3), newState.get(v4)
-                    )
-            );*/
-            PageRank.execute(g, dampingFactor, 0.0);
-        }
-        System.out.println(System.currentTimeMillis() - now);
+        Graph g = new MemoryGraph();
+        Vertex v1 = g.addVertex();
+        Vertex v2 = g.addVertex();
+        Vertex v3 = g.addVertex();
+        g.addEdge(v1, v3);
+        g.addEdge(v2, v3);
+        HITS.execute(g, 0.0);
     }
 }
