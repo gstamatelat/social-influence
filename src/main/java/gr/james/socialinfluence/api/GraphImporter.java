303a455891bbb9dce75e43709bdd91dd71102c80
Giorgos Stamatelatos <gstamatelat@gmail.com>
2015-08-25 14:12:24 +0300

    Default GraphImporter.from with default GraphFactory

--- src/main/java/gr/james/socialinfluence/api/GraphImporter.java
+++ src/main/java/gr/james/socialinfluence/api/GraphImporter.java
@@ -1,8 +1,14 @@
 package gr.james.socialinfluence.api;
 
+import gr.james.socialinfluence.util.Finals;
+
 import java.io.IOException;
 import java.io.InputStream;
 
 public interface GraphImporter {
+    default Graph from(InputStream in) throws IOException {
+        return from(in, Finals.DEFAULT_GRAPH_FACTORY);
+    }
+
     <T extends Graph> T from(InputStream in, GraphFactory<T> factory) throws IOException;
 }
