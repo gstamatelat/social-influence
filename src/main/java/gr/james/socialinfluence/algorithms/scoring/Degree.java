85d78ac699d8c5332e641cfe71e3cbd9f3a8bebc
Giorgos Stamatelatos <gstamatelat@gmail.com>
2015-08-25 14:18:54 +0300

    Add filter to Degree algorithm

--- src/main/java/gr/james/socialinfluence/algorithms/scoring/Degree.java
+++ src/main/java/gr/james/socialinfluence/algorithms/scoring/Degree.java
@@ -4,12 +4,19 @@ import gr.james.socialinfluence.api.Graph;
 import gr.james.socialinfluence.graph.Vertex;
 import gr.james.socialinfluence.util.collections.GraphState;
 
+import java.util.Collection;
+
 public class Degree {
-    public static GraphState<Integer> execute(Graph g, boolean in) {
+    public static GraphState<Integer> execute(Graph g, Collection<Vertex> filter, boolean in) {
         GraphState<Integer> degreesState = new GraphState<>();
-        for (Vertex v : g) {
-            degreesState.put(v, (in) ? g.getInDegree(v) : g.getOutDegree(v));
-        }
+
+        g.getVertices().stream().filter(filter::contains)
+                .forEach(i -> degreesState.put(i, (in) ? g.getInDegree(i) : g.getOutDegree(i)));
+
         return degreesState;
+    }
+
+    public static GraphState<Integer> execute(Graph g, boolean in) {
+        return execute(g, g.getVertices(), in);
     }
 }
