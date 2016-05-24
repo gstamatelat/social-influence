af450e8e566d06f854e5198be8b320efeb6066a4
Giorgos Stamatelatos <gstamatelat@gmail.com>
2016-05-24 17:08:05 +0300

    Update enron file

--- src/main/java/gr/james/influence/main/Enron.java
+++ src/main/java/gr/james/influence/main/Enron.java
@@ -1,6 +1,6 @@
 package gr.james.influence.main;
 
-import gr.james.influence.algorithms.scoring.PageRank;
+import gr.james.influence.algorithms.scoring.Degree;
 import gr.james.influence.api.Graph;
 import gr.james.influence.graph.io.Edges;
 import gr.james.influence.util.collections.GraphState;
@@ -11,7 +11,30 @@ import java.io.IOException;
 public class Enron {
     public static void main(String[] args) throws IOException {
         Graph g = new Edges().from(new FileInputStream("C:\\Users\\James\\Downloads\\graph.csv"));
-        GraphState<Double> pr = PageRank.execute(g, 0.15);
-        System.out.println(pr);
+
+        GraphState<Integer> inDeg = Degree.execute(g, true);
+        GraphState<Integer> outDeg = Degree.execute(g, false);
+
+        // In Degree distribution
+        System.out.println("IN DEGREE");
+        int maxIn = inDeg.values().stream().mapToInt(i -> i).max().getAsInt();
+        int[] distIn = new int[maxIn + 1];
+        for (int d : inDeg.values()) {
+            distIn[d]++;
+        }
+        for (int i = 0; i < distIn.length; i++) {
+            System.out.printf("%d %d%n", i, distIn[i]);
+        }
+
+        // Out Degree distribution
+        System.out.println("OUT DEGREE");
+        int maxOut = outDeg.values().stream().mapToInt(i -> i).max().getAsInt();
+        int[] distOut = new int[maxOut + 1];
+        for (int d : outDeg.values()) {
+            distOut[d]++;
+        }
+        for (int i = 0; i < distOut.length; i++) {
+            System.out.printf("%d %d%n", i, distOut[i]);
+        }
     }
 }
