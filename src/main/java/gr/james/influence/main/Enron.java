8d9a28563e93b77d9a1d747d7864b2f37465db14
Giorgos Stamatelatos <gstamatelat@gmail.com>
2016-05-26 15:47:27 +0300

    Update enron file

--- src/main/java/gr/james/influence/main/Enron.java
+++ src/main/java/gr/james/influence/main/Enron.java
@@ -17,24 +17,29 @@ import java.util.stream.Collectors;
 
 public class Enron {
     public static void main(String[] args) throws IOException {
-        Graph g = new Edges().from(new FileInputStream("C:\\Users\\James\\Downloads\\graph.csv"));
+        Graph r = new Edges().from(new FileInputStream("C:\\Users\\James\\Downloads\\graph.csv"));
 
         String[] guilty = new String[]{"23", "32", "63", "118"};
         String[] potential = new String[]{"8", "43", "45", "55", "60", "62", "140"};
 
         // Convert all edge weights x to 2^(-x)
-        Graph r = new MemoryGraph();
-        for (Vertex v : g) {
-            r.addVertex(v);
+        Graph g = new MemoryGraph();
+        for (Vertex v : r) {
+            g.addVertex(v);
         }
         for (Vertex v : r) {
-            g.getOutEdges(v).entrySet().stream().filter(e -> r.containsVertex(e.getKey()))
-                    .forEach(e -> r.addEdge(v, e.getKey(), 1.0e+11 * Math.pow(2.0, -e.getValue().getWeight())));
+            r.getOutEdges(v).entrySet().stream().filter(e -> r.containsVertex(e.getKey()))
+                    .forEach(e -> g.addEdge(v, e.getKey(), 1.0e+11 * Math.pow(2.0, -e.getValue().getWeight())));
         }
-        for (String m : g.metaKeySet()) {
-            r.setMeta(m, g.getMeta(m));
+        for (String m : r.metaKeySet()) {
+            g.setMeta(m, r.getMeta(m));
         }
-        g = r;
+
+        // Stats
+        System.out.println("EDGES: " + r.getEdgesCount());
+        System.out.println("MEAN DEGREE: " + r.getVertices().stream().map(r::getOutDegree).mapToInt(i -> i).average().getAsDouble());
+        System.out.println("MEAN STRENGTH: " + r.getVertices().stream().map(r::getOutStrength).mapToDouble(i -> i).average().getAsDouble());
+        System.out.println("DIAMETER: " + r.getDiameter());
 
         // In Degree distribution
         GraphState<Integer> inDeg = Degree.execute(g, true);
