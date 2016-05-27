83667ebb7c3d108d3479dc59ad69c4d2d35ccf1e
Giorgos Stamatelatos <gstamatelat@gmail.com>
2016-05-27 23:52:25 +0300

    Update enron file

--- src/main/java/gr/james/influence/main/Enron.java
+++ src/main/java/gr/james/influence/main/Enron.java
@@ -14,8 +14,12 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Enron {
+    private static Graph g = new MemoryGraph();
+    private static Map<VertexPair, Double> distanceMap = new HashMap<>();
+
     public static void main(String[] args) throws IOException {
         Graph r = new Edges().from(new FileInputStream("C:\\Users\\James\\Downloads\\graph.csv"));
 
@@ -23,7 +27,7 @@ public class Enron {
         String[] potential = new String[]{"8", "43", "45", "55", "60", "62", "140"};
 
         // Convert all edge weights x to 2^(-x)
-        Graph g = new MemoryGraph();
+        //Graph g = new MemoryGraph();
         for (Vertex v : r) {
             g.addVertex(v);
         }
@@ -84,55 +88,74 @@ public class Enron {
         }
 
         // Distance matrix
-        Map<VertexPair, Double> distanceMap = Maps.newHashMap(FloydWarshall.execute(g));
+        distanceMap = Maps.newHashMap(FloydWarshall.execute(g));
         for (Vertex v : g) {
             distanceMap.put(new VertexPair(v, v), 0.0);
         }
 
-        // k-center
-        Map<Vertex, Double> kCenter = new HashMap<>();
-        for (Vertex v : g) {
-            double d = guiltyVertices.stream().map(i -> distanceMap.get(new VertexPair(i, v)))
-                    .mapToDouble(Math::log).sum();
-            /*double d = 1 / guiltyVertices.stream().map(i -> 1 / distanceMap.get(new VertexPair(i, v)))
-                    .mapToDouble(i -> i).sum();*/
-            kCenter.put(v, d);
-        }
-        List<Vertex> sortedList = kCenter.entrySet().stream()
-                .sorted((o1, o2) -> Double.compare(o1.getValue(), o2.getValue()))
-                .map(Map.Entry::getKey).collect(Collectors.toList());
-        sortedList.removeAll(guiltyVertices);
+        // Gamma (Experiment 1)
+        System.out.println("# GAMMA (EXPERIMENT 1) #");
+        List<Vertex> gammaList = produceResult(guiltyVertices);
+        evaluate(gammaList, potentialVertices);
 
-        System.out.println("# K-CENTER #");
-        evaluate(sortedList, potentialVertices);
+        // Permutations (Experiment 2)
+        System.out.println("# PERMUTATIONS (EXPERIMENT 2) #");
+        for (int i = 0; i < 4; i++) {
+            for (int j = i + 1; j < 4; j++) {
+                System.out.printf("## PERMUTATION (%d,%d) ##%n", i, j);
+                List<Vertex> inputSet = Arrays.asList(guiltyVertices.get(i), guiltyVertices.get(j));
+                List<Vertex> evalList = new ArrayList<>(guiltyVertices);
+                evalList.removeAll(inputSet);
+                List<Vertex> permutationList = produceResult(inputSet);
+                evaluate(permutationList, evalList);
+            }
+        }
 
         double h = 0;
     }
 
+    private static List<Vertex> produceResult(List<Vertex> S) {
+        Map<Vertex, Double> kCenter = new HashMap<>();
+        for (Vertex v : g) {
+            /*double d = S.stream().map(i -> distanceMap.get(new VertexPair(i, v)))
+                    .mapToDouble(Math::log).sum();*/
+            /*double d = 1 / S.stream().mapToDouble(i -> 1 / distanceMap.get(new VertexPair(i, v))).sum();*/
+            double d = S.stream().mapToDouble(i -> 1 / distanceMap.get(new VertexPair(i, v))).sum();
+            kCenter.put(v, d);
+        }
+        List<Vertex> sortedList = kCenter.entrySet().stream()
+                .sorted((o1, o2) -> -Double.compare(o1.getValue(), o2.getValue()))
+                .map(Map.Entry::getKey).collect(Collectors.toList());
+        sortedList.removeAll(S);
+        return sortedList;
+    }
+
     private static void evaluate(List<Vertex> sortedList, List<Vertex> truth) {
         // indices
-        System.out.printf("INDICES: ");
-        System.out.println(truth.stream().map(sortedList::indexOf).sorted().collect(Collectors.toList()));
+        List<Integer> indices = truth.stream().map(sortedList::indexOf).sorted().collect(Collectors.toList());
+        System.out.printf("Indices: %s - Average Precision: %f%n", indices,
+                IntStream.range(0, indices.size()).mapToDouble(i -> (i + 1.) / (indices.get(i) + 1.))
+                        .average().getAsDouble());
 
         // recall
-        System.out.printf("RECALL: ");
+        System.out.printf("Recall: ");
         int soFar = 0;
         for (int i = 0; i < sortedList.size(); i++) {
             if (truth.contains(sortedList.get(i))) {
                 soFar++;
             }
-            System.out.printf("(%d %f) ", i + 1, (double) soFar / truth.size());
+            System.out.printf("(%d,%f) ", i + 1, (double) soFar / truth.size());
         }
         System.out.println();
 
         // precision
-        System.out.printf("PRECISION: ");
+        System.out.printf("Precision: ");
         soFar = 0;
         for (int i = 0; i < sortedList.size(); i++) {
             if (truth.contains(sortedList.get(i))) {
                 soFar++;
             }
-            System.out.printf("(%d %f) ", i + 1, (double) soFar / (i + 1));
+            System.out.printf("(%d,%f) ", i + 1, (double) soFar / (i + 1));
         }
         System.out.println();
 
@@ -145,7 +168,7 @@ public class Enron {
             }
             double prec = (double) soFar / (i + 1);
             double recall = (double) soFar / truth.size();
-            System.out.printf("(%d %f) ", i + 1, 2 * (prec * recall) / (prec + recall));
+            System.out.printf("(%d,%f) ", i + 1, 2 * (prec * recall) / (prec + recall));
         }
         System.out.println();
     }
