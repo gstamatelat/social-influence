3d930dd09afde95c82e2f9586ec528f4c82f1412
Giorgos Stamatelatos <gstamatelat@gmail.com>
2017-09-13 00:12:01 +0300

    Remove GraphGenerator.decorate(Graph)

--- src/main/java/gr/james/influence/api/GraphGenerator.java
+++ src/main/java/gr/james/influence/api/GraphGenerator.java
@@ -1,7 +1,6 @@
 package gr.james.influence.api;
 
 import gr.james.influence.graph.SimpleGraph;
-import gr.james.influence.util.Conditions;
 import gr.james.influence.util.Finals;
 import gr.james.influence.util.RandomHelper;
 
@@ -11,32 +10,6 @@ import java.util.Random;
  * <p>Represents an entity that can generate a {@code Graph} based on a set of rules.</p>
  */
 public interface GraphGenerator {
-    /**
-     * <p>Decorates a {@code Graph} around a {@code GraphGenerator} so that any subsequent {@code generate} methods will
-     * return a deep copy of {@code g}. In the case of this decorator it is obvious that
-     * {@link #generate(GraphFactory, Random)} is identical to {@link #generate(GraphFactory)} and
-     * {@link #generate(Random)} is identical to {@link #generate()}. The generator is backed by the input graph so that
-     * any changes to {@code g} will reflect on the generator.</p>
-     *
-     * @param g   the graph to decorate around a {@code GraphGenerator}
-     * @param <V> the vertex type
-     * @param <E> the edge type
-     * @return a {@code GraphGenerator} that each of its {@link GraphGenerator#generate()} and
-     * {@link GraphGenerator#generate(GraphFactory)} calls will return a deep copy of the original graph
-     * @throws NullPointerException if {@code g} is {@code null}
-     */
-    static <V, E> GraphGenerator decorate(Graph<V, E> g) {
-        Conditions.requireNonNull(g);
-        return new GraphGenerator() {
-            @Override
-            public <V, E> Graph<V, E> generate(GraphFactory<V, E> factory, Random r) {
-                // return Graphs.deepCopy(g, factory);
-                // TODO
-                throw new UnsupportedOperationException();
-            }
-        };
-    }
-
     /**
      * <p>Generate a new graph of default type based on the rules imposed by this entity. This method is using the
      * global random instance.</p>
