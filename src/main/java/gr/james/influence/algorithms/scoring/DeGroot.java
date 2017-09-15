35a1a2e5bd7701e2dd63592ec30e84cd05192109
Giorgos Stamatelatos <gstamatelat@gmail.com>
2017-09-15 20:08:59 +0300

    Simplify DeGroot

--- src/main/java/gr/james/influence/algorithms/scoring/DeGroot.java
+++ src/main/java/gr/james/influence/algorithms/scoring/DeGroot.java
@@ -5,8 +5,6 @@ import gr.james.influence.api.Graph;
 import gr.james.influence.api.GraphEdge;
 import gr.james.influence.util.collections.GraphState;
 
-import java.util.Map;
-
 public class DeGroot<V> extends AbstractIterativeAlgorithm<V, Double> {
     public static final double DEFAULT_PRECISION = -1.0;
 
@@ -37,15 +35,13 @@ public class DeGroot<V> extends AbstractIterativeAlgorithm<V, Double> {
 
     @Override
     protected GraphState<V, Double> step(Graph<V, ?> g, GraphState<V, Double> previous) {
-        GraphState<V, Double> nextState = GraphState.create();
+        final GraphState<V, Double> nextState = GraphState.create();
         for (V v : g) {
-            double vNewValue = 0.0;
-            for (Map.Entry<V, ? extends GraphEdge<V, ?>> e : g.getOutEdges(v).entrySet()) {
-                vNewValue = vNewValue + (
-                        e.getValue().getWeight() * previous.get(e.getKey())
-                );
+            double w = 0.0;
+            for (GraphEdge<V, ?> e : g.getOutEdges(v).values()) {
+                w += e.getWeight() * previous.get(e.getTarget());
             }
-            nextState.put(v, vNewValue / g.getOutStrength(v));
+            nextState.put(v, w / g.getOutStrength(v));
         }
         return nextState;
     }
