e7206a996170092a8f06f59ff9d541745184b7a6
Giorgos Stamatelatos <gstamatelat@gmail.com>
2017-11-15 17:37:49 +0200

    Add interface VertexScoring

---
+++ src/main/java/gr/james/influence/api/algorithms/VertexScoring.java
@@ -0,0 +1,28 @@
+package gr.james.influence.api.algorithms;
+
+import gr.james.influence.util.collections.GraphState;
+
+/**
+ * Represents an algorithm that assigns a score to a vertex.
+ *
+ * @param <V> the vertex type
+ * @param <T> the score type
+ */
+public interface VertexScoring<V, T> {
+    /**
+     * Get the score assigned to vertex {@code v}.
+     *
+     * @param v the vertex to get the score of
+     * @return the score of vertex {@code v}
+     * @throws NullPointerException     if {@code v} is {@code null}
+     * @throws IllegalArgumentException if {@code v} is not in the graph
+     */
+    T score(V v);
+
+    /**
+     * Get the scores for all vertices.
+     *
+     * @return a {@link GraphState} object holding the scores for all vertices in the graph
+     */
+    GraphState<V, T> scores();
+}
