603500d04678b0d88388a9135fe3c4fecef7b7ee
Giorgos Stamatelatos <gstamatelat@gmail.com>
2017-11-17 23:37:01 +0200

    Generics on ImmutableGraph.decorate

--- src/main/java/gr/james/influence/graph/ImmutableGraph.java
+++ src/main/java/gr/james/influence/graph/ImmutableGraph.java
@@ -16,10 +16,10 @@ public final class ImmutableGraph<V, E> extends GraphDecorator<V, E> {
         super(g);
     }
 
-    public static <V, E> ImmutableGraph decorate(Graph<V, E> g) {
+    public static <V, E> ImmutableGraph<V, E> decorate(Graph<V, E> g) {
         if (g instanceof ImmutableGraph) {
             Finals.LOG.debug("Graph {} is already an instance of ImmutableGraph", g);
-            return (ImmutableGraph) g;
+            return (ImmutableGraph<V, E>) g;
         } else {
             return new ImmutableGraph<>(g);
         }
