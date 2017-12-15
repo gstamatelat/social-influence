7f1c68608db1f33e880b3127897358d08ce4be5f
Giorgos Stamatelatos <gstamatelat@gmail.com>
2017-12-15 14:49:21 +0200

    Add ModifiableGraph and UnmodifiableGraph annotations

---
+++ src/main/java/gr/james/influence/annotation/ModifiableGraph.java
@@ -0,0 +1,15 @@
+package gr.james.influence.annotation;
+
+import gr.james.influence.api.Graph;
+
+import java.lang.annotation.*;
+
+/**
+ * A constructor parameter of type {@link Graph} annotated {@code @ModifiableGraph} indicates that the graph can be
+ * freely mutated after the constructor is invoked. Instance members will reflect changes to it.
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface ModifiableGraph {
+}
---
+++ src/main/java/gr/james/influence/annotation/UnmodifiableGraph.java
@@ -0,0 +1,19 @@
+package gr.james.influence.annotation;
+
+import gr.james.influence.api.Graph;
+
+import java.lang.annotation.*;
+
+/**
+ * A constructor parameter of type {@link Graph} annotated {@code @UnmodifiableGraph} indicates that instances of the
+ * class expect that the graph will not be mutated after the constructor is invoked and will not reflect changes to it.
+ * <p>
+ * Often, this means that the constructor is generating state that relies upon the graph annotated with
+ * {@code UnmodifiableGraph} and mutations to it may result in unexpected behavior when invoking instance members that
+ * utilize that state. In these cases, reliable operation of the instance can be achieved by re-instantiation.
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface UnmodifiableGraph {
+}
