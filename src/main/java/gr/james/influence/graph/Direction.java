7970e11ed1c81f0b3e79b5369e561b4095acb815
Giorgos Stamatelatos <gstamatelat@gmail.com>
2017-12-17 21:28:41 +0200

    Direction javadoc

--- src/main/java/gr/james/influence/graph/Direction.java
+++ src/main/java/gr/james/influence/graph/Direction.java
@@ -1,12 +1,33 @@
 package gr.james.influence.graph;
 
+/**
+ * Represents an edge direction, which can be {@link #INBOUND} or {@link #OUTBOUND}.
+ */
 public enum Direction {
-    INBOUND, OUTBOUND;
+    /**
+     * The inbound direction.
+     */
+    INBOUND,
 
+    /**
+     * The outbound direction.
+     */
+    OUTBOUND;
+
+    /**
+     * Returns {@code true} if this {@link Direction} is {@link #INBOUND}, otherwise {@code false}.
+     *
+     * @return {@code true} if this {@link Direction} is {@link #INBOUND}, otherwise {@code false}
+     */
     public boolean isInbound() {
         return (this == INBOUND);
     }
 
+    /**
+     * Returns {@code true} if this {@link Direction} is {@link #OUTBOUND}, otherwise {@code false}.
+     *
+     * @return {@code true} if this {@link Direction} is {@link #OUTBOUND}, otherwise {@code false}
+     */
     public boolean isOutbound() {
         return (this == OUTBOUND);
     }
