1c17721c0b0123ed5d304b71701b781e38dadbe4
Giorgos Stamatelatos <gstamatelat@gmail.com>
2017-12-24 15:09:10 +0200

    Parameter decay in DecayCentrality (#37)

--- src/main/java/gr/james/influence/algorithms/scoring/DecayCentrality.java
+++ src/main/java/gr/james/influence/algorithms/scoring/DecayCentrality.java
@@ -4,36 +4,41 @@ import gr.james.influence.algorithms.distance.DijkstraShortestPaths;
 import gr.james.influence.annotation.UnmodifiableGraph;
 import gr.james.influence.api.Graph;
 import gr.james.influence.exceptions.IllegalVertexException;
+import gr.james.influence.util.Conditions;
 import gr.james.influence.util.collections.GraphState;
 
 /**
- * Implementation of the <a href="https://en.wikipedia.org/wiki/Closeness_centrality#Variants">decay centrality</a>.
+ * Implementation of the decay centrality from "Social and Economic Networks", ch. 2.2.4.
  * <p>
  * The decay centrality is similar to the {@link ClosenessCentrality closeness centrality}. The decay centrality of
- * vertex {@code v} is defined to be the sum of the reciprocals of the powers of the distances from {@code v} to all
- * other vertices in the graph.
+ * vertex {@code v} is defined to be the sum of the powers of the distances from {@code v} to all other vertices in
+ * the graph.
  * <p>
  * This class is suitable for use in graphs that are not strongly connected.
  * <p>
  * This class uses internal state of size {@code O(V)}.
  *
  * @param <V> the vertex type
- * @see <a href="https://en.wikipedia.org/wiki/Closeness_centrality#Variants">Decay centrality @ Wikipedia</a>
+ * @see "Matthew Jackson. Social and economic networks. Princeton university press, 2010. Chapter 2.2.4."
  */
 public class DecayCentrality<V> extends AbstractSingleVertexScoring<V, Double> {
     private final Graph<V, ?> g;
+    private final double decay;
 
     /**
      * Construct an instance of {@link DecayCentrality} using the specified input {@link Graph} g.
      * <p>
      * The constructor does not perform any calculations and runs in constant time.
      *
-     * @param g the input {@link Graph}
+     * @param g     the input {@link Graph}
+     * @param decay the decay value in (0,1)
      * @throws NullPointerException if {@code g} is {@code null}
      */
-    public DecayCentrality(@UnmodifiableGraph Graph<V, ?> g) {
+    public DecayCentrality(@UnmodifiableGraph Graph<V, ?> g, double decay) {
         super(g);
+        Conditions.requireArgument(decay > 0 && decay < 1, "decay argument must be in (0,1)");
         this.g = g;
+        this.decay = decay;
     }
 
     @Override
@@ -42,7 +47,7 @@ public class DecayCentrality<V> extends AbstractSingleVertexScoring<V, Double> {
         double sum = 0;
         for (V w : g) {
             if (!v.equals(w)) {
-                sum += 1 / Math.pow(2.0, dijkstra.distanceTo(w));
+                sum += Math.pow(decay, dijkstra.distanceTo(w));
             }
         }
         return sum;
