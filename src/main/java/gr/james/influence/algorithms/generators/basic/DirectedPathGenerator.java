fae98482e10071df35a2d6487cdd749cb61d8cc6
Giorgos Stamatelatos <gstamatelat@gmail.com>
2018-01-31 16:23:35 +0200

    Implement DirectedPathGenerator

---
+++ src/main/java/gr/james/influence/algorithms/generators/basic/DirectedPathGenerator.java
@@ -0,0 +1,54 @@
+package gr.james.influence.algorithms.generators.basic;
+
+import gr.james.influence.api.algorithms.GraphGenerator;
+import gr.james.influence.graph.Graph;
+import gr.james.influence.graph.GraphFactory;
+import gr.james.influence.graph.VertexProvider;
+
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * A generator that produces directed path graphs.
+ * <p>
+ * The identification of this generator is
+ * <ul>
+ * <li>source: the vertex on the source tail of the path, which has no incoming edges</li>
+ * <li>target: the vertex on the target tail of the path, which has no outgoing edges</li>
+ * </ul>
+ */
+public class DirectedPathGenerator implements GraphGenerator {
+    private final int vertexCount;
+
+    /**
+     * Constructs a new {@link DirectedPathGenerator}.
+     *
+     * @param vertexCount the vertex count
+     * @throws IllegalArgumentException if {@code vertexCount < 1}
+     */
+    public DirectedPathGenerator(int vertexCount) {
+        if (vertexCount < 1) {
+            throw new IllegalArgumentException();
+        }
+        this.vertexCount = vertexCount;
+    }
+
+    @Override
+    public <V, E> Graph<V, E> generate(GraphFactory<V, E> factory,
+                                       Random r,
+                                       VertexProvider<V> vertexProvider,
+                                       Map<String, V> identification) {
+        final Graph<V, E> g = factory.createGraph(vertexCount);
+
+        V current = g.addVertex(vertexProvider);
+        identification.put("source", current);
+        while (g.vertexCount() < vertexCount) {
+            final V newVertex = g.addVertex(vertexProvider);
+            g.addEdge(current, newVertex);
+            current = newVertex;
+        }
+        identification.put("target", current);
+
+        return g;
+    }
+}
