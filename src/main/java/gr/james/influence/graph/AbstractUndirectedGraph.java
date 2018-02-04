5a619a7a210e4ad61c26140b8a633d5c4ff1b9e1
Giorgos Stamatelatos <gstamatelat@gmail.com>
2018-02-04 12:37:46 +0200

    Implement AbstractUndirectedGraph.toString

--- src/main/java/gr/james/influence/graph/AbstractUndirectedGraph.java
+++ src/main/java/gr/james/influence/graph/AbstractUndirectedGraph.java
@@ -3,7 +3,16 @@ package gr.james.influence.graph;
 abstract class AbstractUndirectedGraph<V, E> implements UndirectedGraph<V, E> {
     @Override
     public String toString() {
-        // TODO
-        throw new UnsupportedOperationException();
+        final StringBuilder sb = new StringBuilder();
+        sb.append(String.format("UndirectedGraph(%d) {%n", this.vertexCount()));
+        for (UndirectedEdge<V, E> e : this.edges()) {
+            if (e.value() == null) {
+                sb.append(String.format("  %s -- %s [%.2f]%n", e.v(), e.w(), e.weight()));
+            } else {
+                sb.append(String.format("  %s -- %s (%s) [%.2f]%n", e.v(), e.w(), e.value(), e.weight()));
+            }
+        }
+        sb.append("}");
+        return sb.toString();
     }
 }
