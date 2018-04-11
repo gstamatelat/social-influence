c98980b0890fd30877019a5a45205fcc23bb38bd
Giorgos Stamatelatos <gstamatelat@gmail.com>
2018-04-11 17:31:55 +0300

    Reformat VertexProvider

--- src/main/java/gr/james/influence/graph/VertexProvider.java
+++ src/main/java/gr/james/influence/graph/VertexProvider.java
@@ -1,6 +1,6 @@
 package gr.james.influence.graph;
 
-import gr.james.influence.algorithms.generators.RandomGraphGenerator;
+import gr.james.influence.algorithms.generators.GraphGenerator;
 import gr.james.influence.util.Conditions;
 
 import java.math.BigInteger;
@@ -11,15 +11,14 @@ import java.util.Set;
 /**
  * Represents an entity that can produce vertices.
  * <p>
- * Normally, this interface is used on graph generators ({@link RandomGraphGenerator}) because generators need to add generic
+ * Normally, this interface is used on graph generators ({@link GraphGenerator}) because generators need to add generic
  * vertices to a graph.
  * <p>
  * A {@code VertexProvider} has the single method {@link #create()} which must create and return a new vertex. Each
  * instance of {@code VertexProvider} must return discrete vertices over its lifetime. More formally, the method
  * {@link #create()} cannot return a vertex that is equal to another one that has already been returned by the same
  * instance. The equality is based on the {@code V.equals} method. If no more discrete vertices can be produced, a
- * {@link java.util.NoSuchElementException} will be raised. Furthermore, this method cannot
- * return {@code null}.
+ * {@link java.util.NoSuchElementException} will be raised. Furthermore, this method cannot return {@code null}.
  *
  * @param <V> the vertex type
  */
@@ -29,8 +28,8 @@ public interface VertexProvider<V> {
      * Package-wide {@link VertexProvider} that returns all the non-negative {@link Integer} values in order (starting
      * from {@code 0}).
      * <p>
-     * The {@link #create()} method will throw {@link NoSuchElementException} if all the possible non-negative
-     * integers are exhausted.
+     * The {@link #create()} method will throw {@link NoSuchElementException} if all the possible non-negative integers
+     * are exhausted.
      */
     VertexProvider<Integer> INTEGER_PROVIDER = new VertexProvider<Integer>() {
         private int nextId = 0;
@@ -66,8 +65,8 @@ public interface VertexProvider<V> {
      * Construct a {@link VertexProvider} that provides elements from a pool.
      * <p>
      * The provider returns the elements with the same order as the {@link Iterator} of {@code pool}. The
-     * {@link #create()} method will throw {@link UnsupportedOperationException} when the elements in {@code pool}
-     * are exhausted.
+     * {@link #create()} method will throw {@link UnsupportedOperationException} when the elements in {@code pool} are
+     * exhausted.
      * <p>
      * Because the resulting {@link VertexProvider} internally uses the {@link Iterator} of {@code pool}, if
      * {@code pool} is modified before the provider is exhausted, a {@link java.util.ConcurrentModificationException}
