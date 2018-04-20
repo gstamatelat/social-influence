54d574f1b8088af18982b5ef39817035eb096dcc
Giorgos Stamatelatos <gstamatelat@gmail.com>
2018-04-20 18:39:21 +0300

    Tweak RandomHelper

--- src/main/java/gr/james/influence/util/RandomHelper.java
+++ src/main/java/gr/james/influence/util/RandomHelper.java
@@ -3,42 +3,43 @@ package gr.james.influence.util;
 import java.util.Random;
 
 /**
- * <p>Singleton class encapsulating a {@link Random} instance. This object is framework-global. You may use it in order
- * to avoid instantiating new {@code Random} objects within your classes.</p>
+ * Singleton class that allows access to a framework-wide {@link Random} instance (the global {@link Random} instance).
+ * <p>
+ * The global {@link Random} can be accessed using the {@link #getRandom()} method. This object will be lazily
+ * initialized the first time it is accessed using the default {@link Random#Random()} constructor. You may also
+ * initialize it manually using the {@link #initRandom(long)} method, which will allow you to set a seed.
  */
 public final class RandomHelper {
-    /**
-     * <p>The internal {@link Random} object of this singleton class.</p>
-     */
     private static Random r = null;
 
     /**
-     * <p>Initialize the global {@link Random} instance with a seed. You may not initialize the global {@code Random}
-     * instance once it has been used at least once and you may only initialize it once per framework instance.</p>
+     * Initialize the global {@link Random} instance with a seed.
+     * <p>
+     * You may not initialize the global {@link Random} instance once it has been accessed and you may only initialize
+     * it once per framework instance.
      *
-     * @param seed the seed to initialize the global {@code Random} with
+     * @param seed the seed to initialize the global {@link Random} with
      * @return {@code true} if the initialization was successful, otherwise {@code false}
      */
     public static boolean initRandom(long seed) {
         if (r == null) {
-            Finals.LOG.info("Initialized global random with seed: {}", seed);
             r = new Random(seed);
             return true;
         } else {
-            Finals.LOG.info("Initialization of global random failed with seed: {}", seed);
             return false;
         }
     }
 
     /**
-     * <p>Retrieve the global {@link Random} instance for use. If the instance hasn't been previously initialized, it
-     * will be initialized before returning with the default {@code Random} constructor.</p>
+     * Retrieve the global {@link Random} instance of the framework.
+     * <p>
+     * If the instance hasn't been previously initialized, it will be initialized before returning with the default
+     * {@link Random} constructor.
      *
-     * @return the global {@code Random} instance
+     * @return the global {@link Random} instance
      */
     public static Random getRandom() {
         if (r == null) {
-            Finals.LOG.info("Automatically initialized global random with unspecified seed");
             r = new Random();
         }
         return r;
