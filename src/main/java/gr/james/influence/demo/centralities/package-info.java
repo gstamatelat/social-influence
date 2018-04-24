212ebd953b6b1ae236bc9c6a83e456efe6ede063
Giorgos Stamatelatos <gstamatelat@gmail.com>
2018-04-24 14:35:18 +0300

    Add package-info for centrality demos

---
+++ src/main/java/gr/james/influence/demo/centralities/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Demonstration of centrality and other scoring algorithms that are available in package
+ * {@link gr.james.influence.algorithms.scoring}.
+ */
+package gr.james.influence.demo.centralities;
