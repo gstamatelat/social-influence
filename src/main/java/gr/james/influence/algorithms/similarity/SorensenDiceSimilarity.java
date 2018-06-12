5e5954ffa6ca7d15246043e34a8d1be708227b43
Giorgos Stamatelatos <gstamatelat@gmail.com>
2018-06-12 21:08:15 +0300

    Remove ModifiableGraph annotation from SorensenDiceSimilarity

--- src/main/java/gr/james/influence/algorithms/similarity/SorensenDiceSimilarity.java
+++ src/main/java/gr/james/influence/algorithms/similarity/SorensenDiceSimilarity.java
@@ -1,7 +1,6 @@
 package gr.james.influence.algorithms.similarity;
 
 import com.google.common.collect.Sets;
-import gr.james.influence.annotation.ModifiableGraph;
 import gr.james.influence.exceptions.IllegalVertexException;
 import gr.james.influence.graph.DirectedGraph;
 import gr.james.influence.util.Conditions;
@@ -34,7 +33,7 @@ public class SorensenDiceSimilarity<V> implements VertexSimilarity<V, Double> {
      * @param g the {@link DirectedGraph} to construct this instance from
      * @throws NullPointerException if {@code g} is {@code null}
      */
-    public SorensenDiceSimilarity(@ModifiableGraph DirectedGraph<V, ?> g) {
+    public SorensenDiceSimilarity(DirectedGraph<V, ?> g) {
         this.g = Conditions.requireNonNull(g);
     }
 
