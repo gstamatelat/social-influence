53ab513a8861936cee170f40925bb325e6e947f9
Giorgos Stamatelatos <gstamatelat@gmail.com>
2018-06-12 18:10:30 +0300

    Remove ModifiableGraph annotation from CosineSimilarity

--- src/main/java/gr/james/influence/algorithms/similarity/CosineSimilarity.java
+++ src/main/java/gr/james/influence/algorithms/similarity/CosineSimilarity.java
@@ -1,7 +1,6 @@
 package gr.james.influence.algorithms.similarity;
 
 import com.google.common.collect.Sets;
-import gr.james.influence.annotation.ModifiableGraph;
 import gr.james.influence.exceptions.IllegalVertexException;
 import gr.james.influence.graph.DirectedGraph;
 import gr.james.influence.util.Conditions;
@@ -33,7 +32,7 @@ public class CosineSimilarity<V> implements VertexSimilarity<V, Double> {
      * @param g the {@link DirectedGraph} to construct this instance from
      * @throws NullPointerException if {@code g} is {@code null}
      */
-    public CosineSimilarity(@ModifiableGraph DirectedGraph<V, ?> g) {
+    public CosineSimilarity(DirectedGraph<V, ?> g) {
         this.g = Conditions.requireNonNull(g);
     }
 
